8c49d034da0f3adfb599605c077b3f3b288782e1
Ace Danger <hpu.presence@gmail.com>
2025-04-03 14:39:24 +0300

    Updated AppDbContext
    Matching entity and table columns

--- DevelopsTodayTask/AppDbContext.cs
+++ DevelopsTodayTask/AppDbContext.cs
@@ -3,15 +3,49 @@ using System.Data.Entity;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<CabTripRecord> Trips { get; set; }
+    public AppDbContext() : base("name=ConnectionString") { }
 
-    public AppDbContext() : base("name=ConnectionString")
-    {
-    }
-
+    // Manually mapping table
     protected override void OnModelCreating(DbModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<CabTripRecord>().ToTable("Sample_Cab_Data");
-        modelBuilder.Entity<CabTripRecord>().HasKey(t => new { t.TpepPickupDatetime, t.TpepDropoffDatetime });
+        modelBuilder.Entity<CabTripRecord>()
+            .ToTable("Sample_Cab_Data")
+            .Property(p => p.TpepPickupDatetime)
+            .HasColumnName("tpep_pickup_datetime");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.TpepDropoffDatetime)
+            .HasColumnName("tpep_dropoff_datetime");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.PassengerCount)
+            .HasColumnName("passenger_count");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.TripDistance)
+            .HasColumnName("trip_distance");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.StoreAndFwdFlag)
+            .HasColumnName("store_and_fwd_flag");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.PULocationID)
+            .HasColumnName("PULocationID");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.DOLocationID)
+            .HasColumnName("DOLocationID");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.FareAmount)
+            .HasColumnName("fare_amount");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .Property(p => p.TipAmount)
+            .HasColumnName("tip_amount");
+
+        modelBuilder.Entity<CabTripRecord>()
+            .HasKey(s => s.Id);
     }
 }
